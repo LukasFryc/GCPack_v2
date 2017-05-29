0cdd18398974cfa44262f440b453e98d352e76c2
andandes <daren@seznam.cz>
2017-05-29 12:58:09 +0200

    Users, dbScript

---
+++ GCPack.Model/Filters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCPack.Model
+{
+    public class Filter
+    {
+        public int Page { get; set; }
+        public int ItemPerPage { get; set; }
+        public string OrderBy { get; set; }
+    }
+
+    public class UserFilter : Filter 
+    {
+    }
+
+}
---
+++ GCPack.Web/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GCPack.Service;
+using GCPack.Model;
+using GCPack.Service.Interfaces;
+
+namespace GCPack.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        readonly IUsersService userService;
+        public UserController(IUsersService userService)
+        {
+            this.userService = userService;
+        }
+
+        public ActionResult Index()
+        {
+            return View(userService.GetUsers(new UserFilter ()));
+        }
+
+        public ActionResult ShowAddUserDialog()
+        {
+            return View("Edit");
+        }
+
+        public ActionResult AddUser(UserModel user)
+        {
+            userService.AddUser(user);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteUser(int userId)
+        {
+            userService.DeleteUser(userId);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
