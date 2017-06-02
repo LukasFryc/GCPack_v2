854f4b81070ae93d256339bdc414bbfe5e9679d6
andandes <daren@seznam.cz>
2017-06-02 17:36:45 +0200

    Documents

--- GCPack.Model/DocumentModel.cs
+++ GCPack.Model/DocumentModel.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace GCPack.Model
 {
+    public enum DocumentAdminType
+    {
+        User,
+        FromDocumentType
+    }
+
     public class DocumentModel
     {
+        public DocumentModel()
+        {
+            Users = new List<UserModel>();
+        }
+
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DocumentAdminType DocumentAdminType { get; set; }
+        public int AdminID { get; set; }
+        public bool CanEdit { get; set; }
+        public bool CanRevision { get; set; }
+        public List<UserModel> Users { get; set; }
     }
 }
--- GCPack.Web/Controllers/DocumentsController.cs
+++ GCPack.Web/Controllers/DocumentsController.cs
@@ -34,5 +34,33 @@ namespace GCPack.Web.Controllers
         }
 
 
+        public ActionResult Save(DocumentModel document, IEnumerable<HttpPostedFileBase> upload)
+        {
+            this.Request.Files[0].SaveAs("");
+            return View("Edit");
+        }
+
+
+        public ActionResult getDocs()
+        {
+            ICollection<DocumentModel> documents = new HashSet<DocumentModel>();
+            documents.Add(new DocumentModel() { Title = "title 1" });
+            documents.Add(new DocumentModel() { Title = "title 2" });
+            documents.Add(new DocumentModel() { Title = "title 3" });
+            return Json(documents, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Edit()
+        {
+            ViewBag.Type = "Editace dokumentu";
+            DocumentModel document = new DocumentModel() {Title = "Dokument 1"};
+            document.Users.Add(new UserModel() { LastName = "Navratil" });
+            document.Users.Add(new UserModel() { LastName = "Fryc" });
+            ViewBag.Documents = document;
+            
+            return View(document);
+        }
+
+
     }
 }
